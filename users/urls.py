6b4b0c1a014cfbae3e64457d2630df12e0b468b8
bellamkonda922 <bkartheek02@gmail.com>
2021-03-17 17:43:37 +0530

    displaying profile image

--- users/urls.py
+++ users/urls.py
@@ -4,12 +4,12 @@ from .import views
 
 urlpatterns = [
       path('', views.home, name='home'),
-      path('login/',views.login,name='login'),
+      path('login',views.login,name='login'),
       path('register',views.register,name='register'),
-      path('logout/',views.logout,name='logout'),
-      path('profile/',views.profiles,name='profile'),
-      path('createdprofile/profileupdate/',views.profileupdate,name='profileupdate'),
-      path('createdprofile/',views.createdprofile,name='createdprofile'),
-      path('alreadythere/',views.alreadythere,name='alreadythere')
+      path('logout',views.logout,name='logout'),
+      path('profile',views.profiles,name='profile'),
+      path('createdprofile/profileupdate',views.profileupdate,name='profileupdate'),
+      path('createdprofile',views.createdprofile,name='createdprofile'),
+      path('alreadythere',views.alreadythere,name='alreadythere')
 ]
 
