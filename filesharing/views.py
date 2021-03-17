28bfa1bcfc670c1c4f317798df023ab494f0473d
Srikar403 <58563346+Srikar403@users.noreply.github.com>
2021-03-17 17:44:02 +0530

    Update views.py

--- filesharing/views.py
+++ filesharing/views.py
@@ -36,4 +36,11 @@ def ousersfile(request,user):
     ruser=User.objects.get(username=user)
     all_files = File.objects.filter(user=ruser)
     context = {'all_files': all_files, 'u': ruser}
-    return render(request, 'filesharing/ousersfile.html', context)    
+    return render(request, 'filesharing/ousersfile.html', context)   
+def delete(request,pk):
+    user = request.user
+    all_files = File.objects.filter(user=user)
+    file=all_files.filter(pk=pk)
+    file.delete()
+
+    return redirect('My_Files')
