71cf2a80fb921499748cdd1c442a279e725f7daf
Srikar403 <58563346+Srikar403@users.noreply.github.com>
2021-03-17 17:46:15 +0530

    Update models.py

--- filesharing/models.py
+++ filesharing/models.py
@@ -17,3 +17,9 @@ class File(models.Model):
 
     def __str__(self):
         return self.name
+    def delete(self, *args, **kwargs):
+        self.file.delete()
+        self.name.delete()
+        super().delete(*args, **kwargs)
+    
+    
