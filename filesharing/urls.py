b59f476e2fd325e4f2da207d07cfcc2303723cfb
kailashnarreddy <70681977+kailashnarreddy@users.noreply.github.com>
2021-04-27 12:13:32 +0530

    uploadfile,linkedupload,delete,index,detail

--- filesharing/urls.py
+++ filesharing/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
 from .import views
 
-
+app_name='filesharing'
 urlpatterns = [
       path('home1',views.home1,name='home1'),
       path('allusers',views.allusers,name='allusers'),
       path('My_Files',views.My_Files,name='My_Files'),
       path('uploadfile',views.uploadfile,name='uploadfile'),
       path('<slug:user>',views.ousersfile,name='ousersfiles'),
-      path('My_Files/<int:pk>',views.delete,name='delete'),
+      path('My_Files/<int:pk>',views.FileDelete.as_view(),name='delete'),
       path('My_Files/makeprivate/<int:pk>',views.makeprivate,name='makeprivate'),
-      path('My_Files/makepublic/<int:pk>',views.makepublic,name='makepublic')
+      path('My_Files/makepublic/<int:pk>',views.makepublic,name='makepublic'),
+      path('uploadlinkedfile/<int:pk>',views.uploadlinkedfile,name='uploadlinkedfile'),
+      path('index', views.index,name='index'),
+      path('<int:folder_id>/',views.detail,name='detail')
+
 ]
