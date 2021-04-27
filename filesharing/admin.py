6a367e19ad53c46397089680c79cde7eba0dee05
Srikar403 <58563346+Srikar403@users.noreply.github.com>
2021-04-27 12:03:53 +0530

    models.py updated

--- filesharing/admin.py
+++ filesharing/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import File
+from .models import File,Folder
 
 admin.site.register(File)
+admin.site.register(Folder)
--- filesharing/forms.py
+++ filesharing/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import File
+from .models import File,Folder
 
 class DocumentForm(forms.ModelForm):
     file = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
     class Meta:
         model = File
         fields = ('file', )
+class FolderUploadForm(forms.ModelForm):
+    file = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True, 'webkitdirectory': True, 'directory': True}))
+
+    class Meta:
+        model = File
+        fields = ['file',]
