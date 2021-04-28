d3b80d3ed8903e75ff713f840b7efb8448ff9174
kailashnarreddy <70681977+kailashnarreddy@users.noreply.github.com>
2021-04-28 11:26:15 +0530

    Update models.py

--- filesharing/models.py
+++ filesharing/models.py
@@ -7,29 +7,38 @@ def user_directory_path(instance, filename):
     return 'user_{0}/{1}'.format(instance.user.id, filename)
 # Create your models here.
 class Folder(models.Model):
-    name = models.CharField(max_length=100,null=True,blank=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE, default=None)
-    linkedfolder=models.ForeignKey("self",on_delete=models.CASCADE,null=True,blank=True)
-    def get_absolute_url(self):
-        return reverse('fileshare:index')
+    name=models.CharField(max_length=100,null=True,blank=True)
+    user=models.ForeignKey(User,on_delete=models.CASCADE, default=None)
+    linkedfolder=models.ForeignKey("self",null=true,on_delete=models.CASCADE,blank=True)
 
     def __str__(self):
-        return self.name
+        return  self.name
+
+    def get_absolute_url(self):
+        return reverse('fileshare:index')
 class File(models.Model):
 
     name = models.CharField(max_length=100,null=True,blank=True)
-    file=models.FileField(upload_to=user_directory_path)
+    file=models.FileField(upload_to=user_directory_path);
     created_on = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE, default=None)
-    folder = models.ForeignKey(Folder, on_delete=models.CASCADE, null=True, blank=True)
+    folder=models.ForeignKey(Folder,null=True,on_delete=models.CASCADE,blank=True)
     isprivate = models.BooleanField(default=False)
     def filename(self):
         return os.path.basename(self.file.name)
 
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        file=File.objects.get(pk=self.pk)
+        folders=file.folder
+        if not folder :
+            return reverse('filesharing:index')
+        else :
+            return reverse('filesharing:detail',kwargs={'folder_id':folders.pk})
+
     def delete(self, *args, **kwargs):
         self.file.delete()
-        self.name.delete()
         super().delete(*args, **kwargs)
+
 
